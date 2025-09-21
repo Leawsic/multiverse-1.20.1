d0f7bf48ec1ffb14786311d90571d67390567348
Leawsic <3775717540@qq.com>
2025-09-21 12:30:15 +0800

    初始化

---
+++ src/main/java/com/leawsic/multiverse/MultiverseClient.java
@@ -0,0 +1,10 @@
+package com.leawsic.multiverse;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiverseClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
